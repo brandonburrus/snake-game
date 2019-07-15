021eebee6c903434f0d4254d6a6d68a79861b8be
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-07-15 16:22:08 -0700

    Added initial state structure and handled player input

--- src/SnakeGameController.tsx
+++ src/SnakeGameController.tsx
@@ -1,8 +1,69 @@
 import React, { Component } from "react";
+import { Observable, Subscription, fromEvent } from "rxjs";
+import SnakeGameState, { PlayerDirection } from "./SnakeGameState";
 
-export interface GameControllerProps {}
+export interface SnakeGameProps {
+    width?: number;
+    height?: number;
+}
 
-export default class App extends Component<GameControllerProps> {
+export default class SnakeGameController extends Component<SnakeGameProps> {
+    private keyboardEvents: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(document, "keydown");
+    private keyboardEventStream?: Subscription;
+    private playerDirection: PlayerDirection = PlayerDirection.UNKNOWN;
+
+    static defaultProps = {
+        width: 10,
+        height: 10,
+    };
+
+    static state: SnakeGameState = {
+        apple: {
+            position: {
+                x: 0,
+                y: 0,
+            },
+        },
+        snake: [],
+        gameBoard: {
+            width: SnakeGameController.defaultProps.width,
+            height: SnakeGameController.defaultProps.height,
+        },
+    };
+
+    public constructor(props: SnakeGameProps) {
+        super(props);
+    }
+
+    public componentDidMount() {
+        this.keyboardEventStream = this.keyboardEvents.subscribe((keyboardEvent: KeyboardEvent) => {
+            switch (keyboardEvent.key.toLowerCase()) {
+                case "arrowup":
+                case "w":
+                    this.playerDirection = PlayerDirection.UP;
+                    break;
+                case "arrowright":
+                case "d":
+                    this.playerDirection = PlayerDirection.RIGHT;
+                    break;
+                case "arrowdown":
+                case "s":
+                    this.playerDirection = PlayerDirection.DOWN;
+                    break;
+                case "arrowleft":
+                case "a":
+                    this.playerDirection = PlayerDirection.LEFT;
+                    break;
+                default:
+                    console.warn("Unknown player input was fired.");
+            }
+        });
+    }
+
+    public componentWillUnmount() {
+        this.keyboardEventStream && this.keyboardEventStream.unsubscribe();
+    }
+
     public render() {
         return (
             <main>
---
+++ src/SnakeGameState.ts
@@ -0,0 +1,31 @@
+export interface Point {
+    x: number;
+    y: number;
+}
+
+export interface SnakePoint extends Point {
+    skipRenderSteps: number;
+}
+
+export interface GameApple {
+    position: Point;
+}
+
+export enum PlayerDirection {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UNKNOWN,
+}
+
+export interface Rect {
+    width: number;
+    height: number;
+}
+
+export default interface SnakeGameState {
+    apple: GameApple;
+    snake: SnakePoint[];
+    gameBoard: Rect;
+}
